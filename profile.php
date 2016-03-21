8379b3fe568e36ad7a95b1ed8241018ed7d62d1d
vanessabergen <vanessabergen>
2016-03-21 18:42:14 -0400

    profile

--- profile.php
+++ profile.php
@@ -1,87 +1,139 @@
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <title>QBnB</title>
-  
-    </head>
-<body>
+ <?php include 'header.php'; ?>
+ 
  <?php
   //Create a user session or resume an existing one
  session_start();
  ?>
+ <?php
+ //check if the user clicked the logout link and set the logout GET parameter
+if(isset($_GET['logout'])){
+	//Destroy the user's session.
+	$_SESSION['member_id']=null;
+	session_destroy();
+}
+ ?>
  
+ <?php include 'navigation.php'; ?>
+ <?php 
+ if(isset($_SESSION['member_id'])){
+
+ include_once 'config/connection.php'; 
+ $query = "SELECT * FROM member WHERE Member_ID=?";
+ $stmt = $con ->prepare($query); 
+ $stmt->bind_Param("i", $_SESSION['member_id']);
+ $stmt->execute();
+ $result = $stmt->get_result();
+ $myrow = $result->fetch_assoc();
+ 
+ }
+ 
+ else {
+	 echo "test";
+	header("Location: index.php");
+	die();
+	}
+	$password = $myrow['password'];
+	$email = $myrow['EMail'];
+	$firstname = $myrow['FName'];
+	$lastname = $myrow['LName'];
+	$pphone = $myrow['PPhone'];
+	$year = $myrow['Year'];
+	$faculty = $myrow['Faculty'];
+	$degree = $myrow['Degree'];	
+?>
+ 
+
  <?php
  
- if(isset($_POST['updateBtn']) && isset($_SESSION['id'])){
+ if(isset($_POST['saveBtn'])&& isset($_SESSION['member_id'])){
   // include database connection
     include_once 'config/connection.php'; 
 	
-	$query = "UPDATE user SET password=?,email=? WHERE id=?";
+	$password = $_POST['password'];
+	$firstname = $_POST['firstname'];
+	$lastname = $_POST['lastname'];
+	$email = $_POST['email'];
+	$pphone = $_POST['pphone'];
+	$year = $_POST['year'];
+	$faculty = $_POST['faculty'];
+	$degree = $_POST['degree']; 
+	
+	$query = "UPDATE member SET FName= '$firstname', LName= '$lastname', EMail= '$email', PPhone='$pphone', Year='$year', Faculty='$faculty', Degree='$degree', password='$password' WHERE Member_ID=?";
+	echo $query;
+	
  
-	$stmt = $con->prepare($query);	$stmt->bind_param('sss', $_POST['password'], $_POST['email'], $_SESSION['id']);
+	$stmt = $con->prepare($query); 
+	$stmt->bind_Param("issssisss", $_SESSION['member_id'], $firstname, $lastname, $email, $pphone, $year, $faculty, $degree, $password); 
+	
 	// Execute the query
         if($stmt->execute()){
-            echo "Record was updated. <br/>";
+            echo "Record was updated";
         }else{
-            echo 'Unable to update record. Please try again. <br/>';
+            echo "Unable to update record";
         }
+	
+	echo $query;
+	echo $firstname;
  }
- 
+  
  ?>
  
- <?php
-if(isset($_SESSION['id'])){
-   // include database connection
-    include_once 'config/connection.php'; 
-	
-	// SELECT query
-        $query = "SELECT id,username, password, email FROM user WHERE id=?";
- 
-        // prepare query for execution
-        $stmt = $con->prepare($query);
-		
-        // bind the parameters. This is the best way to prevent SQL injection hacks.
-        $stmt->bind_Param("s", $_SESSION['id']);
 
-        // Execute the query
-		$stmt->execute();
- 
-		// results 
-		$result = $stmt->get_result();
-		
-		// Row data
-		$myrow = $result->fetch_assoc();
-		
-} else {
-	//User is not logged in. Redirect the browser to the login index.php page and kill this page.
-	header("Location: index.php");
-	die();
-}
 
-?>
- Welcome  <?php echo $myrow['username']; ?>, <a href="login.php?logout=1">Log Out</a><br/>
-<!-- dynamic content will be here -->
-<form name='editProfile' id='editProfile' action='profile.php' method='post'>
-    <table border='0'>
-        <tr>
-            <td>Username</td>
-            <td><input type='text' name='username' id='username' disabled  value="<?php echo $myrow['username']; ?>"  /></td>
-        </tr>
-        <tr>
-            <td>Password</td>
-             <td><input type='text' name='password' id='password'  value="<?php echo $myrow['password']; ?>" /></td>
-        </tr>
-		<tr>
-            <td>Email</td>
-            <td><input type='text' name='email' id='email'  value="<?php echo $myrow['email']; ?>" /></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>
-                <input type='submit' name='updateBtn' id='updateBtn' value='Update' /> 
-            </td>
-        </tr>
-    </table>
-</form>
-</body>
-</html>
+
+
+		<div class="register-page">
+			<h1>Welcome</h1>
+				
+				<div class="row">
+				<div class="col-md-4">
+				<div class="well">
+				<h2>Profile</h2>
+				 <form name='profile' id='profile' action='profile.php' method='post'>
+					<?php if(isset($_POST['editBtn'])):?>
+					
+					
+					<?php else:	?>		
+					<fieldset disabled>
+					<?php endif; ?>
+					
+					<div class="form-group">
+				            <label>Email</label>
+				            <input class="form-control" type='text' name='email' id='email' required="true" value="<?=$email ?>" />
+				     </div>
+				     <div class="form-group">
+				            <label>Password</label>
+				            <input class="form-control" type='password' name='password' id='password' required="true" value="<?=$password ?>" />
+				     </div>
+				     <div class="form-group">
+				            <label>First Name</label>
+				            <input class="form-control" type='text' name='firstname' id='firstname' required="true" value="<?=$firstname?>"/>
+				     </div>
+				     <div class="form-group">
+				            <label>Last Name</label>
+				            <input class="form-control" type='text' name='lastname' id='lastname' required="true" value="<?=$lastname?>" />
+				     </div>
+				     <div class="form-group">
+				            <label>Phone Number</label>
+				            <input class="form-control" type='text' name='pphone' id='pphone' required="true" value="<?=$pphone?>" />
+				     </div>
+				     <div class="form-group">
+				            <label>Graduating Year</label>
+				            <input class="form-control" type='number' name='year' id='year' required="true" value="<?=$year?>" />
+				     </div>
+				     <div class="form-group">
+				            <label>Faculty</label>
+				            <input class="form-control" type='text' name='faculty' id='faculty' required="true" value="<?=$faculty ?>" />
+				     </div>
+				     <div class="form-group">
+				            <label>Degree</label>
+				            <input class="form-control" type='text' name='degree' id='degree' required="true" value="<?=$degree?>" />
+				     </div>
+				      </fieldset>
+					<input class="btn btn-default" type='submit' id='editBtn' name='editBtn' value='Edit' /> 
+					 <input class="btn btn-default" type='submit' id='saveBtn' name='saveBtn' value='Save' /> 
+				</form>
+			</div>
+			</div>
+			</div>
+			</div>
