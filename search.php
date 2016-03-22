6f9ef6aad4eeaa9cf2aee1e27a5d96d2a1c0142f
Chris Cartwright <chris_cart7@hotmail.com>
2016-03-22 11:22:34 -0400

    Search page sort of working

--- search.php
+++ search.php
@@ -10,7 +10,7 @@
  <?php
  //check if the user clicked the logout link and set the logout GET parameter
 if(isset($_GET['logout'])){
-	console.log("after logout in search");
+	
 	//Destroy the user's session.
 	$_SESSION['member_id']=null;
 	session_destroy();
@@ -22,25 +22,19 @@ if(isset($_GET['logout'])){
 <?php
   if(isset($_POST['searchBtn'])){
 	include_once 'config/connection.php'; 
-	 if($_POST['bedroom'] > 0  ) {
-
-	 	echo "<script>console.log('hello');</script>";
-	 }
-	$query = "SELECT * FROM property WHERE Price=? AND district_id=?";// AND type=? AND bedroom=? AND bathroom=? AND Pool=?";
+	 
+	$query = "SELECT * FROM property WHERE Price LIKE '%" . $_POST['price'] . "%' AND District_ID LIKE '%" . $_POST['district'] . "%' AND Type LIKE '%" . $_POST['type'] . "%' AND Bedroom LIKE '%" . $_POST['bedroom'] . "%' AND Bathroom LIKE '%" . $_POST['bathroom'] . "%' AND Pool LIKE '%" . $_POST['Pool'] . "%' AND Laundry LIKE '%" . $_POST['Laundry'] . "%' AND Internet LIKE '%" . $_POST['Internet'] . "%' AND Parking LIKE '%" . $_POST['Parking'] . "%' AND AC LIKE '%" . $_POST['AC'] . "%' AND Heat LIKE '%" . $_POST['Heat'] . "%' AND Gym LIKE '%" . $_POST['Gym'] . "%' AND Pets LIKE '%" . $_POST['Pets'] . "%' AND Smoking LIKE '%" . $_POST['Smoking'] . "%' AND Wheelchair LIKE '%" . $_POST['Wheelchair'] . "%'"; 
 
 
-
-	//(member_id, district_id, address, street_name, postal_code, type, price, bathroom, Pool, Laundry, Internet, Parking, AC, Heat, Gym, Pets, Smoking, Wheelchair, bedroom) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 	if($stmt = $con ->prepare($query)) {
-		$stmt->bind_Param("ii", $_POST['price'], $_POST['district']);//, $_POST['type'], $_POST['bedroom'], $_POST['bathroom'], $_POST['Pool']);
 		$stmt->execute();
 		$result = $stmt->get_result();
- 		$myrow = $result->fetch_assoc();
-		echo $myrow['Type'];
-	die();
+ 		while($myrow = $result->fetch_assoc()) {
+		}
+
 	}
 	else{
-		echo "insert query failed";
+		echo "query failed";
 	}
 	
  }
@@ -60,11 +54,7 @@ if(isset($_GET['logout'])){
 	
 	<div class="form-group">
 			<label>Price Per Week</label>
-		</br>
-			<label>Minimum</label>
-			<input class="form-control" type='number' name='price1' id='price1' placeholder='Minimum'/>
-			<label>Maximum</label>
-			<input class="form-control" type='number' name='price2' id='price2' placeholder='Maximum'/>
+			<input class="form-control" type='number' name='price' id='price' placeholder='Maximum'/>
 	</div>			        
 				            
 	 <div class="form-group">     
@@ -100,6 +90,106 @@ if(isset($_GET['logout'])){
 			<label>Bathroom(s)</label>
 			<input class="form-control" type='number' name='bathroom' id='bathroom' placeholder='2' />
 	</div>
+
+	<div class="form-group">
+			<label>Pool:
+				<select name="Pool">
+					<option value="">Select...</option>
+					<option value="0">No</option>
+					<option value="1">Yes</option>
+				</select>
+			</label>
+	</div>	
+	
+	<div class="form-group">
+			<label>Laundry:
+				<select name="Laundry">
+					<option value="">Select...</option>
+					<option value="0">No</option>
+					<option value="1">Yes</option>
+				</select>
+			</label>
+	</div>	
+	
+	 <div class="form-group">
+			<label>Internet:
+				<select name="Internet">
+					<option value="">Select...</option>
+					<option value="0">No</option>
+					<option value="1">Yes</option>
+				</select>
+			</label>
+	</div>	
+	
+	<div class="form-group">
+			<label>Parking:
+				<select name="Parking">
+					<option value="">Select...</option>
+					<option value="0">No</option>
+					<option value="1">Yes</option>
+				</select>
+			</label>
+	</div>	
+	
+	<div class="form-group">
+			<label>Heat:
+				<select name="Heat">
+					<option value="">Select...</option>
+					<option value="0">No</option>
+					<option value="1">Yes</option>
+				</select>
+			</label>
+	</div>	
+
+	<div class="form-group">
+			<label>AC:
+				<select name="AC">
+					<option value="">Select...</option>
+					<option value="0">No</option>
+					<option value="1">Yes</option>
+				</select>
+			</label>
+	</div>	
+	
+	<div class="form-group">
+			<label>Smoking:
+				<select name="Smoking">
+					<option value="">Select...</option>
+					<option value="0">No</option>
+					<option value="1">Yes</option>
+				</select>
+			</label>
+	</div>	
+	
+	<div class="form-group">
+			<label>Wheelchair:
+				<select name="Wheelchair">
+					<option value="">Select...</option>
+					<option value="0">No</option>
+					<option value="1">Yes</option>
+				</select>
+			</label>
+	</div>	
+
+	<div class="form-group">
+			<label>Gym:
+				<select name="Gym">
+					<option value="">Select...</option>
+					<option value="0">No</option>
+					<option value="1">Yes</option>
+				</select>
+			</label>
+	</div>	
+	
+	<div class="form-group">
+			<label>Pets:
+				<select name="Pets">
+					<option value="">Select...</option>
+					<option value="0">No</option>
+					<option value="1">Yes</option>
+				</select>
+			</label>
+	</div>	
 		
 	
 
@@ -109,6 +199,9 @@ if(isset($_GET['logout'])){
 </form>
 </div>
 </div>
+<div class="cold-md-8">
+	<p></p>
+</div>	
 </div>
 </div>
 
